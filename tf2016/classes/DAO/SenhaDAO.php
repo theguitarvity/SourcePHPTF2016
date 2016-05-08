b00c6ec75f04ad76cc93237122f8854939b78406
Victor Lopes <contato_victorlopes@hotmail.com>
2016-05-07 22:38:15 -0400

    alguns ajustes

--- tf2016/classes/DAO/SenhaDAO.php
+++ tf2016/classes/DAO/SenhaDAO.php
@@ -5,32 +5,31 @@
  *
  * @copyright (c) 2015, ByteGod IT solutions - todos os direitos reservados
  */
-
 require_once("ConnectionFactory.php");
+
 class SenhaDAO {
+
     // criando metodo construtor
-    
+
     function __construct() {
         $this->conn = new ConnectionFactory();
         $this->pdo = $this->conn->Connect();
     }
-    
-    function cadadastrarTF(Senha $senha){
-        try{
-            $cad = $this->pdo->prepare("INSERT INTO senha(senha, idTransformando, fk_senha_apoio1) VALUES(:senha, :tf, :ap)");
+
+    function cadadastrarTF(Senha $senha) {
+        try {
+            $cad = $this->pdo->prepare("INSERT INTO senha(senha, idTransformando, idApoio) VALUES(:senha, :tf, :ap)");
             $param = array(
-                ":senha"=>md5($senha->getSenha()),
-                ":tf"=>$senha->getCodTF(),
-                ":ap"=>0
+                ":senha" => md5($senha->getSenha()),
+                ":tf" => $senha->getCodTF(),
+                ":ap" => $senha->getCodApoio()
             );
             return $cad->execute($param);
-            
-            
         } catch (PDOException $ex) {
-            echo $ex->getMessage();
+            echo $ex->getMessage() . "<br  />";
+            echo $ex->getLine() . "<br  />";
+            echo $ex->getFile() . "<br  />";
         }
     }
-    
-    
-    
+
 }
