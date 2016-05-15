bb29a64b0b42ab2dacdeb0e7255671f069d9158f
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-05-14 21:13:51 -0400

    Versão UI-20160514-MG

--- tf2016/admin.php
+++ tf2016/admin.php
@@ -1,59 +1,107 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Admin - Transformers 2016</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
 
-    <title>Kronos | Login</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="assets/css/signin.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <div class="container">
-    <img src="assets/img/oioi.png" style="width: 200px; height: 200px; margin: 10px 10px 10px 460px;">
-
-      <form class="form-signin">
-        <h2 class="form-signin-heading"</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"><p style="font-size: 12pt;">Mantenha-me conectado</p>
-          </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-      </form>
-
-    </div> <!-- /container -->
+			 <!-- Header -->
+            <header id="header" class="alt">
+                <h1><a href="index.html">NGC</a> Transformers 2016</h1>
+                <nav id="nav">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li>
+                            <a href="#" class="icon fa-angle-down">Menu</a>
+                            <ul>
+                                <li><a href="sobre.html">Sobre</a></li>
+                                <li><a href="inscricao.php">Inscrição</a></li>
+                                <li><a href="elements.html">Elements</a></li>
+                                <li>
+                                    <a href="#">Submenu</a>
+                                    <ul>
+                                        <li><a href="#">Option One</a></li>
+                                        <li><a href="#">Option Two</a></li>
+                                        <li><a href="#">Option Three</a></li>
+                                        <li><a href="#">Option Four</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="#" class="button">Minha Inscrição</a></li>
+                    </ul>
+                </nav>
+            </header>
 
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+			<!-- Main -->
+				<section id="main" class="container">
+					<header>
+						<h2>Administração</h2>
+						<p>Página de administração do Apoio.</p>
+					</header>
+					<div class="box">
+						 <form method="post" action="#">
+										<div class="row uniform 50%">
+											<div class="6u 12u(mobilep)">
+												<input type="email" name="email" id="email" value="" placeholder="Email">
+											</div>
+											<div class="6u 12u(mobilep)">
+												<input type="password" name="password" id="password" value="" placeholder="Password">
+											</div>
+										</div>									
+										
+										<div class="row uniform">
+											<div class="12u">
+												<ul class="actions">
+													<li><input type="submit" value="Entrar"></li>
+													<li><input type="reset" value="Reset" class="alt"></li>
+												</ul>
+											</div>
+										</div>
+									</form>
+
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+					 <ul class="copyright">
+                    <li>&copy; NGC Transformers 2016. Todos os direitos reservados.</li><li>Desenvolvido por: <a href="http://bytegod.com.br">ByteGod IT Solutions</a></li>
+                </ul>
+
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
